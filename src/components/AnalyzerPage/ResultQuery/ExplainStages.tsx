c6f680e16c765ae05e7a62a9f4ed9c449ce10db4
Vladyslav Tymoshenko <vlados230761@gmail.com>
2023-08-29 14:36:35 +0300

    Added a function to create a random background color for the string values of each stages

--- src/components/AnalyzerPage/ResultQuery/ExplainStages.tsx
+++ src/components/AnalyzerPage/ResultQuery/ExplainStages.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 interface lables {
   labels_before: { name: string; value: string }[];
   labels_after: { name: string; value: string }[];
@@ -8,9 +7,18 @@ interface lables {
   filtred_out: boolean;
 }
 
+const stringToColor = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 360));
+  return `hsl(${color}, 90%, 80%)`;
+};
+
 function ExplainStages(props: lables) {
-  const { labels_before, labels_after, line_after, line_before, filtred_out } =
-    props;
+  const { labels_before, labels_after, line_after, filtred_out } = props;
 
   const modifiedLabels = labels_after.filter((labelAfter) => {
     const lablesBefore = labels_before.find(
@@ -19,25 +27,40 @@ function ExplainStages(props: lables) {
     return !lablesBefore || lablesBefore.value !== labelAfter.value;
   });
 
+  const lablesBeforeString = labels_before.map((el, index) => {
+    const labelColor = stringToColor(`${el.name}-${el.value}`);
+    const labelStyle = {
+      backgroundColor: labelColor,
+    };
+    return (
+      <article style={labelStyle} className=" label">
+        {el.name} = {el.value}
+      </article>
+    );
+  });
+  const lablesModifiedString = labels_after.map((el, index) => {
+    const labelColor = stringToColor(`${el.name}-${el.value}`);
+    const labelStyle = {
+      backgroundColor: labelColor,
+    };
+    return (
+      <article style={labelStyle} className=" label">
+        {el.name} = {el.value}
+      </article>
+    );
+  });
+
   return (
     <div className="explain-section__row">
       <div className="explain-section__row-title">
         Available labels on this stage:
       </div>
-      <div className="explain-section__row-body">
-        {' '}
-        {labels_before.map((el, index) => (
-          <p key={index}>
-            {el.name} = {el.value}
-          </p>
-        ))}
-      </div>
+      <div className="explain-section__row-body">{lablesBeforeString}</div>
       <div className="explain-section__row-title">Line after this stage:</div>
       <div className="explain-section__row-body">
         <p className={`line__after ${filtred_out ? 'filtered' : ''}`}>
           {line_after}
         </p>
-
         {filtred_out && (
           <p className="filtred__title">
             the line has been filtered out on this stage
@@ -50,11 +73,7 @@ function ExplainStages(props: lables) {
             Added/Modified labels:
           </div>
           <div className="explain-section__row-body">
-            {modifiedLabels.map((el, index) => (
-              <p key={index}>
-                {el.name} = {el.value}
-              </p>
-            ))}
+            {lablesModifiedString}
           </div>
         </>
       )}
